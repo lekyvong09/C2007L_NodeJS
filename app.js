39c76063da47cdce85e10260ce3de140245279eb
lekyvong09 <lekyvong09@gmail.com>
2022-08-27 21:22:47 +0700

    post data on nodejs

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-
-    if (url === '/post') {
+    const method = req.method;
+    if (url === '/') {
         res.write('<html>');
         res.write('<head><title>My Websit</title></head>');
         res.write('<body><form action="/message" method="POST"><input name="anyName" type="text"/><button type="submit">Send</button></form</body');
         res.write('</html>');
         res.end();
+    } else if (url === '/message' && method === 'POST'){ 
+        const body = [];
+        req.on('data', (chunkData) => {
+            console.log(chunkData);
+            body.push(chunkData);
+        });
+
+        req.on('end', (data) => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                console.log(err);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+            });
+        })        
     } else {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');

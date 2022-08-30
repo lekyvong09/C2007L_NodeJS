828c1df87210e4dbf2b7368fef22f237883cbea3
lekyvong09 <lekyvong09@gmail.com>
2022-08-30 20:31:18 +0700

    expressjs - use vs get vs post

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const app = express();
  */
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
+app.get('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body.title);
     res.redirect('/');
 });
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.send('<h1>Hello from express</h1>');
 });
 

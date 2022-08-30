4fca1d9b0635f08d78814b1062d49faea95b7adc
lekyvong09 <lekyvong09@gmail.com>
2022-08-30 19:49:17 +0700

    module import & export

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./route');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My Websit</title></head>');
-        res.write('<body><form action="/message" method="POST"><input name="anyName" type="text"/><button type="submit">Send</button></form</body');
-        res.write('</html>');
-        res.end();
-    } else if (url === '/message' && method === 'POST'){ 
-        const body = [];
-        req.on('data', (chunkData) => {
-            console.log(chunkData);
-            body.push(chunkData);
-        });
-
-        req.on('end', (data) => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                console.log(err);
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                res.end();
-            });
-        })        
-    } else {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My Websit</title></head>');
-        res.write('<body><h1>Hello World</h1></body');
-        res.write('</html>');
-        res.end();
-    }
-    
+    routes.handler(req, res);
 });
+
 
 server.listen(3001);

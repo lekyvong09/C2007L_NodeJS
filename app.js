c0cd5a069f4d2968fe7a0263941de0e774c4540a
lekyvong09 <lekyvong09@gmail.com>
2022-08-30 21:17:04 +0700

    expressjs - bootstrap@5

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
  * filter / middleware / interceptor
  */
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 
 app.use('/admin', adminRoutes);
 
